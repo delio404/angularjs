d853266cafbedb6d603d9e504d880281c127951c
Adelio Aguirre de Castro <adelio.a.castro@gmail.com>
2021-09-30 11:11:01 -0300

    criando_filtros

--- curso_angular-js/js/filters/nameFilter.js
+++ curso_angular-js/js/filters/nameFilter.js
@@ -1,7 +1,12 @@
 angular.module("listaTelefonica").filter("name",function(){
     return function(input){
-        console.log(input)
-        return input
+        var listaDeNomes=input.split(" ")
+        var listaDeNomesFormatada=listaDeNomes.map(function(nome){
+            if(/(da|de)/.test(nome)) return nome
+            return nome.charAt(0).toUpperCase() + nome.substring(1).toLowerCase()
+        })
+        console.log(listaDeNomesFormatada) 
+        return listaDeNomesFormatada.join(" ")
     }
 
 })
